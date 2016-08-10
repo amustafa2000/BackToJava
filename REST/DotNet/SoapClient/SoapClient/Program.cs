e8aa137036ea1f01ce7bfa6ec6585660c666ce86
amustafa2000 <ahmed.mustafa@winshuttle.com>
2016-08-10 11:54:52 -0700

    CS Examples

---
+++ REST/DotNet/SoapClient/SoapClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SoapWebserviceRef.RestWebservice obj = new SoapWebserviceRef.RestWebservice();
+			Console.WriteLine(" Calling Web Methods");
+			Console.WriteLine("---------------------");
+			Console.WriteLine("\n Calling Hello World");
+			Console.WriteLine("  " + obj.HelloWorld());
+			Console.ReadLine();
+		}
+	}
+}
