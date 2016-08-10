401ba63736a4fde5ef05cf60a9b1cb1160a6d185
amustafa2000 <ahmed.mustafa@winshuttle.com>
2016-08-10 13:24:58 -0700

    Sample anjular code

---
+++ REST/DotNet/CSharpAngularTest/CSharpAngularTest/Scripts/script.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="angular.js" />
+var myModule = angular.module("myModule", []);
+myModule.controller("myController", function ($scope) {
+    $scope.Math = Math;
+    var myArray = [
+        { 'Name': 'Ahmed', 'Color': 'Red', 'img': '/Images/composer.png', 'sval': Math.sin(Math.PI / 2) },
+        { 'Name': 'Mustafa', 'Color': 'Green', 'img': '/Images/composer.png', 'sval': Math.sin(Math.PI / 4) }
+    ];
+    $scope.myArray = myArray;
+});
