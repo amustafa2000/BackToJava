c8db819050ba0a7bfe663f1938b60d341ff90c5b
amustafa2000 <ahmed.mustafa@winshuttle.com>
2016-08-10 14:12:53 -0700

    restructure

---
+++ DotNet/SoapWebService/SoapWebService/SoapWebservice.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="SoapWebservice.asmx.cs" Class="SoapWebService.RestWebservice" %>
---
+++ DotNet/SoapWebService/SoapWebService/SoapWebservice.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SoapWebService
+{
+	/// <summary>
+	/// Summary description for WebService1
+	/// </summary>
+	[WebService(Namespace = "http://sp2013ii/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+	[System.Web.Script.Services.ScriptService]
+	public class RestWebservice : System.Web.Services.WebService
+	{
+
+		[WebMethod]
+		public string HelloWorld()
+		{
+			return "Hello World";
+		}
+	}
+}
