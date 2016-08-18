59f8fb2c38cbf81d4e6b58577b10a7fdd8a67558
amustafa2000 <ahmed.mustafa@winshuttle.com>
2016-08-18 09:21:52 -0700

    Very Simple Example of WinForm

---
+++ DotNet/WinFormExample/WinFormExample/ExampleForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormExample
+{
+	public partial class ExampleForm : Form
+	{
+		public ExampleForm()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.textBox1.Text ="Hellow World";
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			Console.Out.Write("Text Changed");
+		}
+
+		private void ExampleForm_Load(object sender, EventArgs e)
+		{
+			Console.Out.Write("XXXXXXXXXXXXX Form Load Called");
+		}
+	}
+}
