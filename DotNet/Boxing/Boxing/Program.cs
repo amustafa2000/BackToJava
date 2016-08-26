b800a036fd08e86e7227e96345e8278beec461e3
amustafa2000 <ahmed.mustafa@winshuttle.com>
2016-08-25 22:46:41 -0700

    new projects

---
+++ DotNet/Boxing/Boxing/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amdev.com
+{
+	class BoxingAndDynamic
+	{
+		static void Main(string[] args)
+		{
+			
+			MySuperClass superClass = new MySuperClass(1);
+			superClass.ChildrenDoIt(1);
+			Console.WriteLine("superClass.day = " + superClass.day);
+			MySubClass subClass = new MySubClass(2);
+			subClass.ChildrenDoIt(2);
+			subClass.DynamicMethod(1);
+			subClass.DynamicMethod("Really", "x", "y", "z");
+			MySuperClass fromSubClass = subClass;
+			MySubClass sameSubClass = (MySubClass)fromSubClass;
+			// *** dynamic
+			// object x
+			dynamic x = 1;
+			Console.WriteLine("Type now = " + x.GetType());
+			x = "x";
+			Console.WriteLine("Type now = " + x.GetType());
+			x = subClass;
+			x.SubClassMethod();
+			Console.WriteLine("Type now = " + x.GetType());
+
+			int xxxx = 2;
+		}
+	}
+	public class MySuperClass
+	{
+
+		public enum Days { Sun, Mon, tue, Wed, thu, Fri, Sat }; 
+		public int id;
+		public int day;
+		public MySuperClass()
+		{
+		}
+		public MySuperClass(int id)
+		{
+			this.id = id;
+			this.day = (int)Days.Fri;
+		}
+		public virtual void ChildrenDoIt(int x)
+		{
+			Console.WriteLine("In Parent virtual method ");
+		}
+	}
+	public class MySubClass : MySuperClass
+	{
+		public MySubClass()
+		{
+		}
+		public MySubClass(int id)
+		{
+			base.id = id;
+		}
+		public void SubClassMethod()
+		{
+			var x = "";
+		}
+		public void TryEnumInhereit()
+		{
+			Console.WriteLine("Enumeration Cannot be inherited *******");
+			//this.Days
+			// Does not work
+		}
+		public void DynamicMethod(dynamic x, params dynamic [] xx)
+		{
+			Console.WriteLine("the type is = " + x.GetType());
+
+		}
+		public virtual void ChildrenDoIt(int x)
+		{
+			Console.WriteLine("In CHILDREN virtual method ");
+		}
+	}
+
+}
---
+++ DotNet/ParameterPassing/ParameterPassing/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterPassing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ParamPassTest tests = new ParamPassTest();
+			String forOut = "Before";
+			String forOut2 = "Before2";
+			String returned = "retBefore";
+			String refParam = "This a refParam";
+			Console.WriteLine("forOut Before = " + forOut);
+			Console.WriteLine("forOut Before2 = " + forOut2);
+			Console.WriteLine("returned Before = " + returned);
+			Console.WriteLine("refParam Before = " + refParam);
+
+			returned = tests.test(out forOut, "First", "Second", out forOut2, ref refParam);
+			Console.WriteLine("forOut After = " + forOut);
+			Console.WriteLine("forOut2 After = " + forOut2);
+			Console.WriteLine("returned After = " + returned);
+			Console.WriteLine("refParam After = " + refParam);
+			Console.ReadKey();
+		}
+
+	
+	}
+	public class ParamPassTest
+	{
+		public ParamPassTest()
+		{
+
+		}
+
+		public String test(out string forOut, string s, string y, out string forOut2, ref String refParam)
+		{
+			Console.WriteLine("got this s = " + s);
+			Console.WriteLine("got this y = " + y);
+			forOut = "Changed forOut";
+			forOut2 = "Changed forOut2";
+			refParam = "Not output, But changed!!!!!!!";
+			return "xxxx";
+		}
+	}
+
+}
