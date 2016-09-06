73d6949cee6a91a86c98a39abd7568db1f173190
amustafa2000 <ahmed.mustafa@winshuttle.com>
2016-09-05 18:08:52 -0700

    latest

---
+++ DotNet/Learning/ConstVSReadOnly/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVSReadOnly
+{
+	class Program
+	{
+		public const int aTob = 100;//Compile time constant
+		public static readonly int ro; // Runtime constant Can wait till runtime DEFAULT CONFIG
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(aTob);
+			Console.WriteLine(ro);
+		}
+	}
+}
