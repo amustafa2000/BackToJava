e58583472f70d7d3e01435edece238d5496b600b
amustafa2000 <ahmed.mustafa@winshuttle.com>
2016-09-21 21:51:42 -0700

    Latest

---
+++ DotNet/Learning/TypeExample/TypeExampleProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeExample
+{
+	class TypeExampleProgram
+	{
+		static void Main(string[] args)
+		{
+			var bird = new Animal<Egg>();
+			var pig = new Animal<Piglet>();
+		}
+	}
+
+	public class Animal<T> where T : Offspring
+	{
+		public T Offspring { get; set; }
+	}
+
+	public abstract class Offspring { }
+	public class Egg : Offspring { }
+	public class Piglet : Offspring { }
+}
