776b2935cbaae8486b8714c704fd01323d11ffe4
amustafa2000 <ahmed.amustafa@gmail.com>
2016-09-22 07:51:54 -0700

    latest

--- DotNet/Learning/TypeTest/Program.cs
+++ DotNet/Learning/TypeTest/Program.cs
@@ -10,6 +10,30 @@ namespace TypeTest
 	{
 		static void Main(string[] args)
 		{
+			Dog dog = new Dog();
+			Dog dog2 = new Dog(20000);
+			Type t1 = dog.GetType();
+
+			Console.WriteLine(dog is Animal);
+			Console.WriteLine(dog is Dog);
+			Console.ReadKey();
+		}
+	}
+
+	public class Animal
+	{
+		
+	}
+	public class Dog : Animal
+	{
+		private int zip;
+		public Dog(int zip = 10000)
+		{
+			this.zip = zip;
+		}
+		public int GetZip()
+		{
+			return zip;
 		}
 	}
 }
