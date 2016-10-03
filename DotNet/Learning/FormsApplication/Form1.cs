e109f8cf8e58a0615096327643a67369fdcda0dc
amustafa2000 <ahmed.amustafa@gmail.com>
2016-10-03 15:48:18 -0700

    Latest

---
+++ DotNet/Learning/FormsApplication/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsApplication
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Console.WriteLine("User Name = " + this.UserName.Text);
+			MessageBox.Show("User Name = " + this.UserName.Text);
+		}
+
+		private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+		{
+
+		}
+	}
+}
