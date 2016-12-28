c02d29e94a90257d763ecf465c1a1db3d9a4787f
amustafa2000 <ahmed.amustafa@gmail.com>
2016-12-27 21:53:23 -0800

    Latest

---
+++ DotNet/Learning/ScrambleString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrambleString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StringBuilder jumbleSB = new StringBuilder();
+			jumbleSB.Append("ABC");
+			for (int i = 0; i < jumbleSB.Length; i++)
+			{
+				
+				Random random = new Random();
+				int indexFirst = random.Next() % jumbleSB.Length;
+				int indexNext = random.Next() % jumbleSB.Length;
+				char temp = jumbleSB[indexFirst];
+				jumbleSB[indexFirst] = jumbleSB[indexNext];
+				jumbleSB[indexNext] = temp;
+
+
+			}
+			Console.WriteLine(jumbleSB);
+			Console.ReadKey();
+		}
+	}
+}
