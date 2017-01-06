92605790619414eaf1d59911f0895a46df748f81
amustafa2000 <ahmed.amustafa@gmail.com>
2017-01-05 21:04:07 -0800

    removed extra imports

--- Java/MavenJAX-RS-Jersey/src/main/java/test/service/DBService.java
+++ Java/MavenJAX-RS-Jersey/src/main/java/test/service/DBService.java
@@ -1,9 +1,7 @@
 package test.service;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
